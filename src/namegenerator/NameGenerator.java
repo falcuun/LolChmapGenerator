9b88201b1f168d373a80b992e3c64e4e3f79a25d
Falcuun Falcuun <Falcuun Falcuun>
2017-02-09 13:40:19 +0100

    Second commit

---
+++ src/namegenerator/NameGenerator.java
@@ -0,0 +1,15 @@
+package namegenerator;
+
+/**
+ *
+ * @author Falcuun
+ */
+public class NameGenerator {
+
+    public static void main(String[] args) {
+        Randomizer randdd = new Randomizer();
+        randdd.setLocation(500, 350);
+        randdd.setVisible(true);
+    }
+
+}
